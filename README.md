43b26f062eba3057bdd30c507a7236379ac9fbea
nazephyrus <126683488+naden01@users.noreply.github.com>
2024-11-26 16:31:37 -0800

    release PBRP tree

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# tecno_LH8n-pbrp
- pbrp tree for LH8n
+# Android device tree for TECNO TECNO LH8n (TECNO-LH8n)
+
+# Clone
+    git clone https://github.com/naden01/tecno_LH8n.git -b 14 device/tecno/LH8n
+
+# Build
+    export ALLOW_MISSING_DEPENDENCIES=true; . build/envsetup.sh; lunch twrp_LH8n-eng; mka vendorbootimage
---
+++ pb_LH8n.mk
@@ -0,0 +1,39 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Enable project quotas and casefolding for emulated storage without sdcardfs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
+# Enable Virtual A/B OTA
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
+
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/pb/config/common.mk)
+
+# Inherit from LH8n device
+$(call inherit-product, device/tecno/LH8n/device.mk)
+
+PRODUCT_DEVICE := LH8n
+PRODUCT_NAME := twrp_LH8n
+PRODUCT_BRAND := TECNO
+PRODUCT_MODEL := TECNO LH8n
+PRODUCT_MANUFACTURER := tecno
+
+PRODUCT_GMS_CLIENTID_BASE := android-tecno
+
+# Hide Reflash TWRP & FUSE passthrough
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.twrp.vendor_boot=true \
+    persist.sys.fuse.passthrough.enable=true
